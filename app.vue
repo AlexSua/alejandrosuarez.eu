3541e5598b097a1d1a23a7233284b6e14c30f849
Alejandro Suárez <AlexSua@users.noreply.github.com>
2022-07-20 18:34:01 +0200

    fix: added key to routing

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <router-view v-slot="{ Component }">
     <transition :name="transitionName" @before-leave="onBeforeLeave" @after-enter="() => onAfterEnter()">
-        <component :is="Component" v-model:afterEnter="afterEnter" />
+        <component :is="Component" v-model:afterEnter="afterEnter"  :key="$route.path" />
     </transition>
   </router-view>
 </template>
