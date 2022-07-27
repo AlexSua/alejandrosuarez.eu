bdf864474a947d7f142124305f19c9651bb46de2
Alejandro Suárez <AlexSua@users.noreply.github.com>
2022-07-27 12:01:49 +0200

    fix: radio button modelValue fix

--- components/videochat/RadioList.vue
+++ components/videochat/RadioList.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col w-full px-5 ">
         <h3 class="text-vanilla-yellow  border-b-light-50 border-solid border-b-[2.5px]"> {{title}}</h3>
         <div class="field-radiobutton m-2 mt-2" v-for="item,index in items" :key="index+item.label">
-            <RadioButton v-model="modelValue" :id="item.label" :name="title" :value="item.label" :disabled="disabled" class="mx-2" />
+            <RadioButton v-model="radioValue" :id="item.label" :name="title" :value="item.label" :disabled="disabled" class="mx-2" />
             <label :for="item.label">{{item.label}}</label>
         </div>
     </div>
@@ -19,9 +19,16 @@ const props = defineProps<{
 }>()
 
 const emits = defineEmits(["update:modelValue"])
-watch(()=>props.modelValue, (value: any) => {
-    emits("update:modelValue", value)
-})
+
+const radioValue = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value) {
+    emits("update:modelValue", value);
+  },
+});
+
 </script>
 
 <style lang="scss">
