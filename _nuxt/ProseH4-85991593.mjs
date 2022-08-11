35332a367ee8bff1d0acd121002e511a3ac38e48
AlexSua <8196195+AlexSua@users.noreply.github.com>
2022-08-11 23:33:51 +0000

    Deploying to gh-pages from @ AlexSua/PaginaPersonalPrivate@4f2a939d9c8258145a02c55f7707090f5b379339 🚀

---
+++ _nuxt/ProseH4-85991593.mjs
@@ -0,0 +1,1 @@
+import{e as n,o as a,a as s,P as r,N as c,r as d,O as _}from"./entry-2b318d32.mjs";const i=["id"],f=n({__name:"ProseH4",props:{id:null},setup(e){return(t,l)=>{const o=_;return a(),s("h4",{id:e.id},[r(o,{href:`#${e.id}`},{default:c(()=>[d(t.$slots,"default")]),_:3},8,["href"])],8,i)}}});export{f as default};
