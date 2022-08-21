6cd5bc7e8af61adb887b448b17857d0feca3531f
Alejandro Suárez <AlexSua@users.noreply.github.com>
2022-08-21 03:29:46 +0200

    fix: changing the number of servers to 4 to improve discovery speed

--- utils/webrtc.ts
+++ utils/webrtc.ts
@@ -44,9 +44,9 @@ export default class WebRtcConnection {
             {
                 urls: "stun:openrelay.metered.ca:80",
             },
-            {
-                urls: 'stun:stun.l.google.com:19302'
-            },
+            // {
+            //     urls: 'stun:stun.l.google.com:19302'
+            // },
             {
                 urls: "turn:openrelay.metered.ca:80",
                 username: "openrelayproject",
