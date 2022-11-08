743f6605f5e8afbcc10325d50c349c0b3ab7cdd0
AlexSua <8196195+AlexSua@users.noreply.github.com>
2022-11-08 18:37:12 +0000

    Deploying to gh-pages from @ AlexSua/alejandrosuarez.eu@8538c07e86b61dc9008586b1b730c22cc2019a23 🚀

--- _nuxt/ProseBlockquote-5a3be86a.mjs
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper-84c1d017.mjs";import{o as r,a as t,r as a}from"./entry-1515c3ba.mjs";const c={};function s(e,l){return r(),t("blockquote",null,[a(e.$slots,"default")])}var _=o(c,[["render",s]]);export{_ as default};
