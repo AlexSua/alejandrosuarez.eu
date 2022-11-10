c11fe927cfd6cc4974557888debcf62ecbe8d162
AlexSua <8196195+AlexSua@users.noreply.github.com>
2022-11-10 17:47:19 +0000

    Deploying to gh-pages from @ AlexSua/alejandrosuarez.eu@36d5a10d6ac7909634c2e08d17e68b61c06e4334 🚀

---
+++ _nuxt/useRouteNoLanguage-22f27895.mjs
@@ -0,0 +1,1 @@
+import{D as e,z as t}from"./entry-e4a9c400.mjs";const r=()=>{const a=e();return t(()=>a.params.lang?a.path.replace("/"+a.params.lang,""):a.path)};export{r as u};
--- _nuxt/useRouteNoLanguage-c95f8ef6.mjs
+++
@@ -1,1 +0,0 @@
-import{D as e,z as t}from"./entry-7db80cea.mjs";const r=()=>{const a=e();return t(()=>a.params.lang?a.path.replace("/"+a.params.lang,""):a.path)};export{r as u};
