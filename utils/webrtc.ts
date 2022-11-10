36d5a10d6ac7909634c2e08d17e68b61c06e4334
Alejandro Suárez <AlexSua@users.noreply.github.com>
2022-11-10 18:45:14 +0100

    update in turn and stun servers.

--- utils/webrtc.ts
+++ utils/webrtc.ts
@@ -53,9 +53,9 @@ export default class WebRtcConnection {
 	}
 
 	private _stun_servers = [
-		// {
-		// 	urls: 'stun:stun.l.google.com:19302'
-		// },
+		{
+			urls: 'stun:stun.l.google.com:19302'
+		},
 		{
 			urls: "stun:openrelay.metered.ca:80",
 		},
@@ -67,11 +67,11 @@ export default class WebRtcConnection {
 			username: "openrelayproject",
 			credential: "openrelayproject",
 		},
-		// {
-		// 	urls: "turn:openrelay.metered.ca:80",
-		// 	username: "openrelayproject",
-		// 	credential: "openrelayproject",
-		// },
+		{
+			urls: "turn:openrelay.metered.ca:80",
+			username: "openrelayproject",
+			credential: "openrelayproject",
+		},
 	]
 
 	private configuration = {
