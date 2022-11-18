0d72c5f4056180add2c7900423020efe4cb02eae
Alejandro Suárez <AlexSua@users.noreply.github.com>
2022-11-18 05:13:38 +0100

    only openrelay stun server

--- utils/webrtc.ts
+++ utils/webrtc.ts
@@ -53,9 +53,9 @@ export default class WebRtcConnection {
 	}
 
 	private _stun_servers = [
-		{
-			urls: 'stun:stun.l.google.com:19302'
-		},
+		// {
+		// 	urls: 'stun:stun.l.google.com:19302'
+		// },
 		{
 			urls: "stun:openrelay.metered.ca:80",
 		},
