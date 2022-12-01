973a9f20491972e166a9090009ba8cbd8fb75314
AlexSua <8196195+AlexSua@users.noreply.github.com>
2022-12-01 19:14:25 +0000

    Deploying to gh-pages from @ AlexSua/alejandrosuarez.eu@1d5826790dcd000986b3147541e4fd94e38fa363 🚀

---
+++ _nuxt/ProseTr-7ee5e10c.mjs
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-84c1d017.mjs";import{o,a as t,r as a}from"./entry-cdc870d1.mjs";const s={};function n(r,c){return o(),t("tr",null,[a(r.$slots,"default")])}var _=e(s,[["render",n]]);export{_ as default};
